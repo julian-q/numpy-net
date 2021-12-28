c61c6e7dfde96e27c47d3331c7208f65a5d21737
julian-q <julianhquevedo@gmail.com>
2021-12-27 16:42:07 -0800

    made cache and gradients intermediate variables

--- model.py
+++ model.py
@@ -28,47 +28,49 @@ class NeuralNetwork:
             cache[f'Z{i + 1}'], cache[f'A{i + 1}'] = Z, A
 
         if training:
-            self.cache = cache
+            return A, cache
+        else:
+            return A
 
-        return A
-
-    def backward(self, X, Y):
-        self.gradients = {}
+    def backward(self, X, Y, cache):
+        gradients = {}
 
         for i, layer in reversed(list(enumerate(self.layers))):
             if i == len(self.layers) - 1:
-                A = self.cache[f'A{i + 1}']
+                A = cache[f'A{i + 1}']
                 dA = self.loss.derivative(Y, A)
             else:
-                dZ_next = self.gradients[f'dZ{i + 2}']
+                dZ_next = gradients[f'dZ{i + 2}']
                 W_next = self.parameters[f'W{i + 2}']
                 dA = dZ_next @ W_next.T
 
-            Z = self.cache[f'Z{i + 1}']
+            Z = cache[f'Z{i + 1}']
             dZ = dA * layer['activation'].derivative(Z)
 
-            self.gradients[f'dA{i + 1}'], self.gradients[f'dZ{i + 1}'] = dA, dZ
+            gradients[f'dA{i + 1}'], gradients[f'dZ{i + 1}'] = dA, dZ
 
-            A_prev = X if i == 0 else self.cache[f'A{i}']
+            A_prev = X if i == 0 else cache[f'A{i}']
             N = Y.shape[0]
 
             dW = (A_prev.T @ dZ) / N
             db = np.sum(dZ, axis=0) / N
 
-            self.gradients[f'dW{i + 1}'], self.gradients[f'db{i + 1}'] = dW, db
+            gradients[f'dW{i + 1}'], gradients[f'db{i + 1}'] = dW, db
+
+        return gradients
 
     def train(self, X, Y, n_iterations, alpha):
-        self.forward(X, training=True)
+        _, cache = self.forward(X, training=True)
         losses = []
 
         for epoch in range(n_iterations):
-            self.backward(X, Y)
+            gradients = self.backward(X, Y, cache)
             
             for i in range(len(self.layers)):
-                self.parameters[f'W{i + 1}'] -= alpha * self.gradients[f'dW{i + 1}']
-                self.parameters[f'b{i + 1}'] -= alpha * self.gradients[f'db{i + 1}']
+                self.parameters[f'W{i + 1}'] -= alpha * gradients[f'dW{i + 1}']
+                self.parameters[f'b{i + 1}'] -= alpha * gradients[f'db{i + 1}']
 
-            Y_hat = self.forward(X, training=True)
+            Y_hat, cache = self.forward(X, training=True)
 
             epoch_loss = np.sum(self.loss.evaluate(Y, Y_hat))
             accuracy = acc(Y, Y_hat)
